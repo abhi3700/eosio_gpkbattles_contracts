2eda6b34bc826becb308471d5637875713a418df
Abhijit Roy <dalvath3700@gmail.com>
2021-03-14 13:08:25 +0530

    report_123 - DONE. TODO: report_456

--- programs/recreate.sh
+++ programs/recreate.sh
@@ -1,6 +1,7 @@
 # RECREATE 
 # means creating the testing game environment
 # - token transfer
+# - cards transfer
 # - select cards
 # - pair w player
 # - play
@@ -14,6 +15,16 @@ cleoswt push action eosio.token transfer '["gbuser111114", "gpkbattlesc1", "1.00
 cleoswt push action eosio.token transfer '["gbuser111115", "gpkbattlesc1", "1.00000000 WAX", "transfer 1 WAX fee for playing game"]' -p gbuser111115@active
 cleoswt push action eosio.token transfer '["gbuser111121", "gpkbattlesc1", "1.00000000 WAX", "transfer 1 WAX fee for playing game"]' -p gbuser111121@active
 cleoswt push action eosio.token transfer '["gbuser111122", "gpkbattlesc1", "1.00000000 WAX", "transfer 1 WAX fee for playing game"]' -p gbuser111122@active
+
+# cards transfer from simpleassets to gpkbatescrow
+cleoswt push action simpleassets transfer '{"from": "gbuser111111", "to": "gpkbatescrow", "assetids": ["100000000007690", "100000000007692", "100000000007720"], "memo": "transfer cards for gpk.battles playing"}' -p gbuser111111@active
+cleoswt push action simpleassets transfer '{"from": "gbuser111112", "to": "gpkbatescrow", "assetids": ["100000000007693", "100000000007695", "100000000007697"], "memo": "transfer cards for gpk.battles playing"}' -p gbuser111112@active
+cleoswt push action simpleassets transfer '{"from": "gbuser111113", "to": "gpkbatescrow", "assetids": ["100000000007714", "100000000007716", "100000000007722"], "memo": "transfer cards for gpk.battles playing"}' -p gbuser111113@active
+cleoswt push action simpleassets transfer '{"from": "gbuser111114", "to": "gpkbatescrow", "assetids": ["100000000007691", "100000000007702", "100000000007711"], "memo": "transfer cards for gpk.battles playing"}' -p gbuser111114@active
+cleoswt push action simpleassets transfer '{"from": "gbuser111115", "to": "gpkbatescrow", "assetids": ["100000000007694", "100000000007712", "100000000007721"], "memo": "transfer cards for gpk.battles playing"}' -p gbuser111115@active
+cleoswt push action simpleassets transfer '{"from": "gbuser111121", "to": "gpkbatescrow", "assetids": ["100000000007705", "100000000007707", "100000000007727"], "memo": "transfer cards for gpk.battles playing"}' -p gbuser111121@active
+cleoswt push action simpleassets transfer '{"from": "gbuser111122", "to": "gpkbatescrow", "assetids": ["100000000007706", "100000000007710", "100000000007729"], "memo": "transfer cards for gpk.battles playing"}' -p gbuser111122@active
+
 
 # select cards
 cleoswt push action gpkbattlesc1 sel3card '["gbuser111111", "simpleassets", "100000000007690", "100000000007692", "100000000007720"]' -p gbuser111111@active
